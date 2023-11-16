9903e3c3e7d52847d57aedb0f0aaea249ce9ca03
WangTao <tomwtg@gmail.com>
2023-11-16 16:03:36 +0800

    Update winwin.py

--- winwin.py
+++ winwin.py
@@ -2,7 +2,7 @@ import streamlit as st
 import  random
 
 txt = st.text_area(
-    "Text to analyze",
+    "请输入人员名单，从",
     "It was the best of times, it was the worst of times, it was the age of "
     "wisdom, it was the age of foolishness, it was the epoch of belief, it "
     "was the epoch of incredulity, it was the season of Light, it was the "
