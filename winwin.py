aeabdbd53241de35b885be40d18799b35ba9d291
WangTao <tomwtg@gmail.com>
2023-11-16 16:18:16 +0800

    Update winwin.py

--- winwin.py
+++ winwin.py
@@ -31,8 +31,9 @@ num_winners = number
 
 
 
+image='https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80'
 
-
+st.image(image)
 
 
 st.button("Reset", type="primary")
