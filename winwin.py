5cfe70934616cb6cde9ecf1a7397cfe4db329cff
WangTao <tomwtg@gmail.com>
2023-11-16 16:19:02 +0800

    Update winwin.py

--- winwin.py
+++ winwin.py
@@ -31,8 +31,7 @@ num_winners = number
 
 
 
-image='https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80'
-
+image='https://img95.699pic.com/element/40106/6543.png_860.png'
 st.image(image)
 
 
